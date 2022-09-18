b596380be8465d98ff60db7947c0acda3e91091a
«Andrey» <khomehko@mail.ru>
2022-09-18 16:53:35 +0300

    тест

---
+++ FinalHW/test/Program.cs
@@ -0,0 +1,24 @@
+﻿int f = 0;
+Console.WriteLine("Введите количество элементов массива, которые будете вводить далее :");
+//  int z = Console.ReadLine();      
+  int z = int.Parse(Console.ReadLine());           
+            string[] array = new string[z];
+ Index(array,z,f);
+
+    static string[] Index(string[] array,int z,int f)
+        {
+             f = 0;
+            do
+            {
+                if (f < z)
+                {
+                  Console.Write($"Введите  {z+1-z+f}-е значение массива " );
+                    // array[f] = int.Parse(Console.ReadLine());
+                     array[f] = Console.ReadLine();
+                   f++;
+                }
+            } while (f < z);
+            return array; 
+        }
+        Console.WriteLine("Значения по порядку которые ввел пользователь :");
+        Console.WriteLine(String.Join(", ",array));
